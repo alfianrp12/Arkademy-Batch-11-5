674993bed59e56cf66293e657611c8368086ce19
Alfian R P <alfianrp12@gmail.com>
2019-07-27 13:58:26 +0700

    Add files via upload

---
+++ ArkademySoal3.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alfian R P
+ */
+import java.util.Scanner;
+public class ArkademySoal3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.println("Pola Gambar");
+        System.out.println("-----------");
+        System.out.print("Panjang : ");
+        int n = in.nextInt();
+        if (n%2 == 1){
+            System.out.println("");
+            cetak_gambar(n);
+            System.out.println("");
+        } else {
+            System.out.println("Angka harus ganjil. Mohon ulangi.");
+        }
+    }
+    static void cetak_gambar(int p){
+        int i, j, k;
+        int median = (p+1)/2;
+        for (i=1;i<=p;i++){
+            if (i == median){
+                for (k=1;k<=p;k++){
+                    System.out.print("* ");
+                }
+            } else {
+                    for (j=1;j<=p;j++){
+                    if (j==1 || j==p){
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("= ");
+                    }
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
